3afe1fb413d71525c418a07cc752e85ce76d6716
Aleix Pol Gonzalez <aleix.pol_gonzalez@mercedes-benz.com>
2025-05-28 17:15:14 +0200

    client: Introduce BlurBehindMask
    Adds another QQuickItem that can be used to mask with a provided image
    rather than from an in-memory render.
    
    Signed-off-by: Georg Leugner <georg.leugner@mbition.io>
    Signed-off-by: Falko Becker <falko.becker@mbition.io>
    Signed-off-by: Julius Range-Luedemann <julius.range-luedemann@mercedes-benz.com>
    Signed-off-by: Falko Becker <falko.becker@mercedes-benz.com>, MBition
    Signed-off-by: Julius Range-Luedemann <julius.range-luedemann@mercedes-benz.com>, MBition
    Signed-off-by: Falko Becker <falko.becker@mercedes-benz.com>, MBition
    Signed-off-by: Julius Range-Luedemann <julius.range-luedemann@mercedes-benz.com>, MBition
    
    Signed-off-by: Georg Leugner <georg.leugner@mercedes-benz.com>, MBition
    Signed-off-by: Falko Becker <falko.becker@mercedes-benz.com>, MBition
    Signed-off-by: Julius Range-Luedemann <julius.range-luedemann@mercedes-benz.com>, MBition

--- src/client/blurbehindmask.h
+++ src/client/blurbehindmask.h
@@ -4,9 +4,9 @@
     SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
-#ifndef BLURBEHIND_H
-#define BLURBEHIND_H
+#pragma once
 
+#include <QImage>
 #include <QQmlParserStatus>
 #include <QQuickItem>
 #include <QtQmlIntegration>
@@ -14,15 +14,16 @@
 class BlurManager;
 class BlurMask;
 
-class BlurBehind : public QQuickItem
+class BlurBehindMask : public QQuickItem
 {
     Q_OBJECT
-    QML_NAMED_ELEMENT(BlurBehind)
+    QML_NAMED_ELEMENT(BlurBehindMask)
     QML_ADDED_IN_VERSION(1, 0)
     Q_PROPERTY(bool activated READ activated WRITE setActivated NOTIFY activatedChanged)
+    Q_PROPERTY(QString maskPath READ maskPath WRITE setMaskPath NOTIFY maskPathChanged)
 public:
-    BlurBehind(QQuickItem *target = nullptr);
-    ~BlurBehind() override;
+    BlurBehindMask(QQuickItem *target = nullptr);
+    ~BlurBehindMask() override;
 
     void componentComplete() override;
     bool activated() const { return m_activated; }
@@ -34,18 +35,21 @@ public:
         refresh();
         Q_EMIT activatedChanged();
     }
+
+    QString maskPath() const { return m_maskPath; }
+    void setMaskPath(const QString &maskPath);
+
 Q_SIGNALS:
     void activatedChanged();
+    void maskPathChanged();
 
 protected:
-    void itemChange(ItemChange change, const ItemChangeData &value) override;
     void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
 
 private:
     void refresh();
     bool m_completed = true;
     bool m_activated = true;
+    QString m_maskPath;
     std::unique_ptr<BlurMask> m_mask;
 };
-
-#endif
---
+++ tests/blurBehindMaskCircle.qml
@@ -0,0 +1,38 @@
+/*
+    SPDX-FileCopyrightText: 2023 Aleix Pol Gonzalez <aleix.pol_gonzalez@mercedes-benz.com>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+import QtQuick
+import QtQuick.Window
+import QtQuick.Controls as QQC2
+
+import org.kde.blurng as BNG
+
+Window {
+    width: 200; height: 200
+    flags: Qt.WA_TranslucentBackground
+    color: "transparent"
+    Component.onCompleted: {
+        showFullScreen()
+    }
+
+    BNG.BlurBehindMask {
+        id: blur
+        x: 10; y: 10
+        width: 500; height: 500
+        maskPath: "happymask.png"
+
+        Drag.active: dragArea.drag.active
+        Drag.hotSpot.x: width / 2
+        Drag.hotSpot.y: height / 2
+
+        MouseArea {
+            id: dragArea
+            anchors.fill: parent
+
+            drag.target: parent
+        }
+    }
+}
